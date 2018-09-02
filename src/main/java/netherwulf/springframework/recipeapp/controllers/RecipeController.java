9b003bc6ee0d6f7d4794da306c71dadf7e7a0475
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-02 22:10:22 +0200

    Added exception handler and view for NotFoundException

--- src/main/java/netherwulf/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/netherwulf/springframework/recipeapp/controllers/RecipeController.java
@@ -2,13 +2,13 @@ package netherwulf.springframework.recipeapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import netherwulf.springframework.recipeapp.commands.RecipeCommand;
+import netherwulf.springframework.recipeapp.exceptions.NotFoundException;
 import netherwulf.springframework.recipeapp.services.RecipeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -58,5 +58,17 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+        log.error("Handling Not Found Exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
