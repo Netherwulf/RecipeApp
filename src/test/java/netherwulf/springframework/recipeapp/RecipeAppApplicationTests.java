0d8125a6b186d7af21cbcea1b10e135e02e29a6a
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-05 14:46:20 +0200

    Fixed integration tests for UnitOfMeasureRepository
    Fixed integration tests for UnitOfMeasureRepository

--- src/test/java/netherwulf/springframework/recipeapp/RecipeAppApplicationTests.java
+++ src/test/java/netherwulf/springframework/recipeapp/RecipeAppApplicationTests.java
@@ -4,12 +4,12 @@ package netherwulf.springframework.recipeapp;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataMongoTest
 public class RecipeAppApplicationTests {
 
     @Test
--- src/test/java/netherwulf/springframework/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/netherwulf/springframework/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,5 +1,6 @@
 package netherwulf.springframework.recipeapp.repositories;
 
+import netherwulf.springframework.recipeapp.bootstrap.RecipeBootstrap;
 import netherwulf.springframework.recipeapp.domain.UnitOfMeasure;
 import netherwulf.springframework.recipeapp.repositories.UnitOfMeasureRepository;
 import org.junit.Before;
@@ -7,6 +8,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,19 +16,29 @@ import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by jt on 6/17/17.
- */
-@Ignore
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
     @Before
     public void setUp() throws Exception {
+
+        recipeRepository.deleteAll();
+        unitOfMeasureRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+        RecipeBootstrap recipeBootstrap = new RecipeBootstrap(categoryRepository, recipeRepository, unitOfMeasureRepository);
+
+        recipeBootstrap.onApplicationEvent(null);
     }
 
     @Test
